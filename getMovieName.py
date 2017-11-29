699747473ab7894ad7b5cc660ee55beca864eaa4
rmmody <rishimmody@gmail.com>
2017-11-29 15:25:16 -0500

    Made changes in script

--- getMovieName.py
+++ getMovieName.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from collections import defaultdict
+from sets import Set
 
 '''- get movies' names of review
 - store names in a dataframe
@@ -11,25 +12,35 @@ from collections import defaultdict
 - or scrape data from the website'''
 
 movie_sentiment_score = defaultdict(list)
-PATH_TO_URL = 'C:\\Users\\buihu\Documents\\2017_2018 Umass Amherst\\Intro to NLP\\Final project\\Data\\aclImdb_v1\\aclImdb\\train\\urls_neg.txt'
+PATH_TO_URL = '/Users/Rishi/Desktop/Study/Fall2017/NLP/Project/Movie-Revenue-Prediction_Sentiment-Analysis/aclImdb/train/urls_neg.txt'
 
-def getMovieName(filename):
+def getMovieName(filename, start, begin):
     i=0
-    f= open("urls_neg_train_movie_name.txt","w+")
+    f= open("urls_neg_train_movie_name.txt","a")
+
+    review_ids = Set()
     for line in open(filename, 'r'):
+    	review_id = line.split("/")[4]
+        review_ids.add(review_id)
+    
+    for id in list(review_ids)[start:begin]:
+    	line = "http://www.imdb.com/title/" + id + "/reviews"
         i+=1
-        print("loading ", i)
+        #print("loading ", i)
         page = requests.get(line)
         soup = BeautifulSoup(page.content, 'html.parser')
         title = soup.find('title').get_text()
         title_list = title.split('Reviews')
-        f.write(' %s\n' % title_list[0])
-        if i==8740:
-            break
+        print title_list[0]
+        f.write(' %s\n' % title_list[0].encode("utf-8"))
+        # if i==8740:
+        #     break
         #movie_sentiment_score['movie'].append(title_list[0])
     f.close()
     print("finish")
+ 
 
-
-getMovieName(PATH_TO_URL)
-print(movie_sentiment_score['movie'][:,50])
+for i in range(1, 34):
+	start = i*80
+	getMovieName(PATH_TO_URL, start, start+ 81)
+#print(movie_sentiment_score['movie'][:,50])
