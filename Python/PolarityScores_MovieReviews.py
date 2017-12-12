ee6b254eca3252766ac996f0cec1132d57aa3866
Lopamudra Pal <lpal@umass.edu>
2017-12-11 22:32:10 -0500

    Add files via upload

--- Python/PolarityScores_MovieReviews.py
+++ Python/PolarityScores_MovieReviews.py
@@ -18,44 +18,87 @@ neg_train = os.path.join(TRAIN_DATA_PATH, "neg")
 
 iteration = 1
 vader_scores = defaultdict(float)
-#===========================Scores generated for the positive review files============================
-for filename in os.listdir(pos_train):
-    file = open(os.path.join(pos_train, filename), 'r')
-    review = file.read()
-    file.close()
-    sid = SentimentIntensityAnalyzer()
-    ss = sid.polarity_scores(review)
-    vader_scores[filename] = ss['compound']  # store the compound score into the vader score dictionary
-    # for k in sorted(ss):
-    #     # print ('{0}: {1}, '.format(k, ss[k]), end='')
-    #     #print('{0}: {1}, '.format(k, ss[k]))
-    #     if k == 'compound':
-    #         vader_scores[filename] = ss[k]     #store the compound score into the vader score dictionary
-
-    if iteration == 5:
-        break
-    iteration += 1
+#files = os.listdir(pos_train)
+# f1 = open("movie_reviews_polarity_score_train_pos.txt","a")
+# #===========================Scores generated for the positive review files============================
+# for filename in files:
+#     file = open(os.path.join(pos_train, filename), 'r')
+#     review = file.read()
+#     file.close()
+#     sid = SentimentIntensityAnalyzer()
+#     ss = sid.polarity_scores(review)
+#     vader_scores[filename] = ss['compound']  # store the compound score into the vader score dictionary
+#
+# for key, value in vader_scores.iteritems():
+#     f1.write(str(key) + " : " + str(value) + "\n" )
+# f1.close()
 #===========================Scores generated for the negative review files============================
 iteration = 0
 files = os.listdir(neg_train)
+f2 = open("movie_reviews_polarity_score_train_neg.txt","a")
+vader_scores_train_neg = defaultdict(float)
 for filename in files:
     file = open(os.path.join(neg_train, filename), 'r')
     review = file.read()
     file.close()
     sid = SentimentIntensityAnalyzer()
     ss = sid.polarity_scores(review)
-    vader_scores[filename] = ss['compound']  # store the compound score into the vader score dictionary
-    # for k in sorted(ss):
-    #     # print ('{0}: {1}, '.format(k, ss[k]), end='')
-    #     #print('{0}: {1}, '.format(k, ss[k]))
-    #     if k == 'compound':
-    #         vader_scores[filename] = ss[k]     #store the compound score into the vader score dictionary
-
-    if iteration == 5:
-        break
+    vader_scores_train_neg[filename] = ss['compound']  # store the compound score into the vader score dictionary
     iteration += 1
 
+for key, value in vader_scores_train_neg.iteritems():
+    f2.write(str(key) + " : " + str(value) + "\n" )
+f2.close()
+# f = open("movie_reviews_polarity_score.txt","w")
+# f.write(str(vader_scores))
 
-print vader_scores
-f = open("movie_reviews_polarity_score.txt","w")
-f.write(str(vader_scores))
+# pos_test = os.path.join(TEST_DATA_PATH, "pos")
+# neg_test = os.path.join(TEST_DATA_PATH, "neg")
+#
+# iteration = 1
+# vader_scores = defaultdict(float)
+# files = os.listdir(pos_test)
+# f = open("movie_reviews_polarity_score_test_pos.txt","a")
+# #===========================Scores generated for the positive review files============================
+# for filename in files:
+#     file = open(os.path.join(pos_test, filename), 'r')
+#     review = file.read()
+#     file.close()
+#     sid = SentimentIntensityAnalyzer()
+#     ss = sid.polarity_scores(review)
+#     vader_scores[filename] = ss['compound']  # store the compound score into the vader score dictionary
+#     #f.writerow([filename] + vader_scores[filename])
+#     #f.write(str(filename)+ ":" + str(vader_scores))
+#
+#     # if iteration == 5:
+#     #     break
+#     iteration += 1
+# for key, value in vader_scores.iteritems():
+#     f.write(str(key) + " : " + str(value) + "\n")
+# f.close()
+# #===========================Scores generated for the negative review files============================
+# iteration = 0
+# files = os.listdir(neg_test)
+# vader_scores_neg = defaultdict(float)
+# #f = open("movie_reviews_polarity_score.txt","w")
+# f_neg = open("movie_reviews_polarity_score_test_neg.txt","a")
+# for filename in files:
+#     file = open(os.path.join(neg_test, filename), 'r')
+#     review = file.read()
+#     file.close()
+#     sid = SentimentIntensityAnalyzer()
+#     ss = sid.polarity_scores(review)
+#     vader_scores_neg[filename] = ss['compound']  # store the compound score into the vader score dictionary
+#     iteration += 1
+#     # if iteration == 5:
+#     #     break
+#
+#     #f.writerow([filename] + vader_scores[filename])
+#     #f.write(str(filename) + ":" + str(vader_scores))
+# for key, value in vader_scores_neg.iteritems():
+#     f_neg.write(str(key) + " : " + str(value) + "\n" )
+#
+# f_neg.close()
+# f.write(str(vader_scores))
+#
+# writer.writerow([key] + value)
